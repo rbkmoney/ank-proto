5a30ef4b37ca83b53d6209e01efbb3ce5d2630ab
Alexandra Usacheva <lazercore@me.com>
2019-11-19 17:10:21 +0300

    pdl added to RussianIndividualEntity

--- proto/questionary.thrift
+++ proto/questionary.thrift
@@ -507,6 +507,8 @@ struct RussianIndividualEntity {
     15: optional PropertyInfoDocumentType property_info_document_type
     16: optional list<BeneficialOwner> beneficial_owners
     17: optional bool has_beneficial_owners
+    18: optional bool pdl_category
+    19: optional string pdl_relation_degree
 }
 
 union IndividualEntity {
